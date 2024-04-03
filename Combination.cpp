0fef6980d2912320317f56bde6ce35fe4df6c75e
mobslayer202 <gavinlyuan@gmail.com>
2024-04-03 10:30:58 -0700

    Clean up Combination.cpp

--- Combination.cpp
+++ Combination.cpp
@@ -1,7 +1,5 @@
 #include "Combination.hpp"
 
-#include <iostream> // Can include twice?
-
 Combination::Combination(short num1s){
     reset(num1s);
 }
@@ -14,18 +12,17 @@ void Combination::reset(short num1s){
     this->combo -= 1;
 }
 
+// Increments to the next combo bigger than current one
 void Combination::operator++(){
+    // Get vector | each value has only one of the 1s in the combo
     std::vector<short> all1s = find1s(); // move elsewhere?, will disappear if use iterator only?
     short newCombo = 0;
 
-    /*std::vector<short>::iterator all1sIt = all1s.begin();
-    while (all1sIt != all1s.end()){
-        if (*all1sIt << 1 != *std::next(all1sIt)){
-            
-        }
-    }*/
-
+    // Go through each of the slices of combo
     for (short i=0; i<all1s.size(); i++){
+        // If the current 1 bit is at the end or if its not going to overlap the 1 bit ahead of it
+        // Move it left
+        // Also move everything behind it back to smallest position.
         if (i+1 == all1s.size() or all1s[i] << 1 != all1s[i+1]){
             all1s[i] <<= 1;
 
@@ -39,11 +36,10 @@ void Combination::operator++(){
 
     }
 
+    // Add the changed slices back together to form the new combo
     for (auto bit:all1s){
-        //std::cout << bit << ",";
         newCombo += bit;
     }
-    //std::cout << "\n";
 
     this->combo = newCombo;
 }
@@ -51,7 +47,11 @@ void Combination::operator++(){
 // Return vector | : For each set bit of combo, returns a short with only that bit set. LSB->MSB
 std::vector<short> Combination::find1s(){
     std::vector<short> all1s;
+    // Calculate number of bits
     short numBits = sizeof(this->combo)*8;
+    
+    // Search whole bit string, if you see a 1: 
+    //      make a bit string with only that 1 and add to vector
     short finder = 0b0000'0000'0000'0001; // Need to change if variable combo size
     for (short i=0; i<numBits; i++){
         if ((finder & this->combo) != 0){ // PARENTHESIS AROUND (finder & this->combo) FIXED BUG!!!
